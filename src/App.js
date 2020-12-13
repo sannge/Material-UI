6ac1cd4ddcbc00939c7ef7bc07e282f339e5af12
San Nge <samngestep@gmail.com>
2020-12-12 23:40:21 -0800

    added makestyles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Typography } from "@material-ui/core";
+import { red } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+	// root: {
+	// 	backgroundColor: "red",
+	// 	color: (props) => props.color,
+	// },
+	helloThereStyles: {
+		fontStyle: "oblique",
+		color: (props) => props.color,
+	},
+	buttonStyles: {
+		border: 0,
+		color: "black",
+		"&:hover": {
+			background: "red",
+		},
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//custom hook that uses makeStyles.
+	const classes = useStyles({ color: "red" });
+
+	return (
+		<>
+			<Typography className={classes.helloThereStyles} variant='h1'>
+				Hello There
+			</Typography>
+			<Button
+				className={classes.buttonStyles}
+				color='secondary'
+				variant='outlined'
+				disableElevation>
+				THIS IS OUR FIRST BUTTON
+			</Button>
+		</>
+	);
 }
 
 export default App;
