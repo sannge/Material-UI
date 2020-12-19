8a09b3ced2832f8b8b8b16b04f1fda5a0e8db6aa
San Nge <samngestep@gmail.com>
2020-12-18 23:27:35 -0800

    changed theme

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,10 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import purple from "@material-ui/core/colors/purple";
-import green from "@material-ui/core/colors/green";
+import blue from "@material-ui/core/colors/blue";
 
 const theme = createMuiTheme({
-	palette: {},
+	palette: {
+		primary: blue,
+	},
 	status: {
 		danger: "orange",
 	},
